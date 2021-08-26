3c727ae1fb4b7681a635de8eb64092eac32a02dd
Michael Hua <huayl.michael@gmail.com>
2021-08-26 11:57:46 -0400

    Finish remote log process from frontend

--- src/front-log/front-log.controller.ts
+++ src/front-log/front-log.controller.ts
@@ -1,16 +1,23 @@
 import { Body, Controller, Inject, Post } from '@nestjs/common';
 import { Logger } from 'winston';
 import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
+import * as fs from 'fs';
 
 import { NGXLogInterface } from './frontLogInterface';
 
 @Controller('')
 export class FrontLogController {
-
   constructor(@Inject(WINSTON_MODULE_PROVIDER) private logger: Logger) {}
 
   @Post('api/log')
   postLog(@Body() logInterface: NGXLogInterface) {
-    return null;
+    fs.writeFile(
+      'frontendError.log',
+      `level: ${logInterface.level}, timestamp: ${logInterface.timestamp}, message: ${logInterface.message}`,
+      { flag: 'a' },
+      function (err) {
+        if (err) this.logger.error(`Write frontend error file failed: ${err}`);
+      },
+    );
   }
 }
