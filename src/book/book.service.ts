f2051f64f74c4e63281b5185539acff6fb2f90a6
Michael Hua <huayl.michael@gmail.com>
2021-10-30 23:02:10 -0400

    V0.98

--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -86,7 +86,7 @@ export class BookService {
       desc: createBookDto.desc,
       keywords: createBookDto.keywords,
       creator: createBookDto.creator,
-      isActive: createBookDto.isActive.toLowerCase() === 'true' ? true : false,
+      isActive: createBookDto.isActive.toLowerCase() === 'active' ? true : false,
       createDate: new Date(),
       readTimes: 0,
       readDuration: 0,
